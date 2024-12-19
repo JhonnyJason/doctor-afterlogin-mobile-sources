de02c592f88d124e1709e0b380ac5f1a724e9265
Lenard Frommelt <lenny09918050@gmail.com>
2024-12-20 01:20:13 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -4,7 +4,6 @@ module.exports = {
             "dayjs": "^1.11.13",
             "gridjs": "^6.0.6",
             // "gridjs-selection": "^4.0.0",
-            "libsodium-wrappers": "^0.7.15",
 	        "thingy-byte-utils":"^0.0.8"
         }
     }
